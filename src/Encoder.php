528fc39d311da08d6422261e433198b363c9c7b3
Mariam Bouzid <mariam.bouzid@icloud.com>
2016-04-28 23:04:09 +0200

    Update Encoder.php
    Addition of bounds() , substitutions() and encode() methods.

--- src/Encoder.php
+++ src/Encoder.php
@@ -22,4 +22,58 @@ class Encoder {
   public function __construct(CSP $csp) {
     $this->csp = $csp;
   }
+  
+  /**
+   * Substitutes primitive comparision for boolean variables
+   * E.g. : 'x <= c' is replaced by 'p(x,c)'
+   * Referring to the a. part of 'Encodage'
+   * @return array of array of string
+   */
+  public function substitutions() {
+  	$predicates = array();
+  	try {
+		foreach ($this->csp->getClauses() as $clauses)
+	  		foreach($clauses->getFNC() as $clause) {
+	  			$predicate = array();
+	  			foreach($clause as $pc) {
+		  			$x = $pc->getLinearExpression()->getTerm(0)->getVar()->getName();
+		  			$c = $pc->getConst();
+		  			$sign = $pc->getSign();
+		  			$predicate[] = ( $sign ? "" : "-")."p(".$x.",".$c.")";
+		  			/* TODO
+		  			 * check if 'p' is not expended yet
+		  			 */
+		  		}
+		  		$predicates[] = $predicate ;
+		  	}
+  	} catch (Exception $e) {
+  		throw $e ;
+  	}
+  	return $predicates;
+  }
+  
+  /**
+   * Determines the bounds axioms
+   * Referring to the b. part , particularly the 8th definition 
+   * @return array of array of string
+   */
+  public function bounds() {
+  	$bounds = array();
+  	foreach ($this->csp->getVariables() as $var) {
+  		$lower = "-p(".$var->getName().",".($var->getLowerBound()-1).")";
+  		$upper = "p(".$var->getName().",".$var->getUpperBound().")";
+  		$bounds[] = [ $lower ];
+  		$bounds[] = [ $upper ];
+  	}
+  	return $bounds;
+  }
+  
+  /**
+   * Gives a sort of SAT representation of the CSP
+   * @return array of array of string
+   */
+  public function encode() {
+  	return array_merge($this->substitutions(), $this->bounds());
+  }
+  
 }
