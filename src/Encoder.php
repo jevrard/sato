6dff7999277e68fe38d09b0a152ec72e3e6fdf91
jevrard <evrardjustine35@gmail.com>
2016-04-28 18:57:06 +0200

    Addition of class Encoder's layout

---
+++ src/Encoder.php
@@ -0,0 +1,25 @@
+<?php
+require_once 'src/SAT.php';
+require_once 'src/CSP.php';
+
+/**
+ * Encodes CSP into SAT problem
+ * @author Justine Evrard
+ * @author Mariam Bouzid
+ */
+class Encoder {
+
+  /**
+   * CSP problem to encode
+   * @var CSP
+   */
+  private $csp;
+
+  /**
+   * Initializes internal state of Encoder object.
+   * @param CSP $csp
+   */
+  public function __construct(CSP $csp) {
+    $this->csp = $csp;
+  }
+}
