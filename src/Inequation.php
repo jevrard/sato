18254493eaf2dd2156ad24f120765505e1e5dba4
jevrard <evrardjustine35@gmail.com>
2016-04-28 02:25:16 +0200

    Creation of class Inequation

---
+++ src/Inequation.php
@@ -0,0 +1,99 @@
+<?php
+require_once 'LinearExpression.php';
+
+/**
+ * Represents an inequation, linear in a first time
+ * Form: f(u) <= c, where u is a vector like (x_1, ..., x_n) and c is a constant integer
+ * @author Justine Evrard
+ * @author Mariam Bouzid
+ */
+class Inequation {
+
+  /**
+   * Left-hand side
+   * @var LinearExpression
+   */
+  private $linearE;
+
+  /**
+   * Right-hand side
+   * @var int
+   */
+  private $const;
+  /**
+   * Logical sign of the Inequation object
+   * i.e. 0 is negative form, 1 is positive form (default)
+   * @var boolean
+   */
+  private $sign;
+
+  /**
+   * Initializes internal state of Inequation object.
+   * @param LinearExpression $expression
+   * @param int $const
+   * @param boolean $sign
+   */
+  public function __construct(LinearExpression $expression, $const, $sign = 1) {
+    $this->linearE = $expression;
+    $this->const = $const;
+    $this->sign = $sign;
+  }
+
+  /**
+   * Tests if @this has only on term on the left
+   * @return boolean
+   */
+  public function isOneTerm() {
+    return $this->linearE->getNumberOfTerms() == 1;
+  }
+
+  /**
+   * Tests if @this is a primitive comparison
+   * @return boolean
+   */
+  public function isPrimitiveComparison() {
+    return $this->isOneTerm() && $linearE->getTerm(0)->getCoeff() == 1 ;
+  }
+
+  /**
+   * Gives the FNC of primitive comparaisons (x <= c)
+   * Corresponds to proposition 1
+   * @return array of array of Inequation
+   */
+  public function getFNC() {
+    $fnc = array();
+    $n = $this->linearE->getNumberOfTerms();
+    $sum = $this->const-$n+1; //c-n+1
+    $combinaisons = LinearExpression::computeCombinaisons($this->linearE->getTermsDomain(), array());
+    foreach($combinaisons as $combi) {
+      if(array_sum($combi) != $sum) continue;
+      $clause = array();
+      for($i=0; $i<$n; $i++) {
+        $term = $this->linearE->getTerm($i);
+        $expression = new LinearExpression(array($term), array($term->getVar()));
+        $ineq = new Inequation($expression, $combi[$i]);
+        $ineq->hashTranslation();
+        $clause[] = $ineq;
+      }
+      $fnc[] = $clause;
+    }
+    return $fnc;
+  }
+
+  /**
+   * Compute the hash translation, given in proposition 1, of one term inequation
+   * i.e. transforms one term inequation into primitive comparison
+   */
+  private function hashTranslation() {
+    if(!$this->isOneTerm()) throw new Exception("Inequation object : impossible to apply hashTranslation() on a non one term inequation.");
+    $term = $this->linearE->getTerm(0);
+    $this->linearE = array(new LinearTerm($term->getVar(), 1));
+    $a = $term->getCoeff();
+    $q = (float)$this->const/(float)$a;
+    if($a > 0) $this->const = (int) floor($q);
+    else {
+      $this->const = (int) ceil($q)-1;
+      $this->sign = 0;
+    }
+  }
+}
